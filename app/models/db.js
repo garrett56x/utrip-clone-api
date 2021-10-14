a3ead510c1f5a7899499784a7b2c3c66153ac3ff
Garrett <garrett56x@gmail.com>
2021-10-14 12:17:59 -0700

    Using dotenv instead of a config file

--- app/models/db.js
+++ app/models/db.js
@@ -1,12 +1,11 @@
 const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-    host: dbConfig.HOST,
-    user: dbConfig.USER,
-    password: dbConfig.PASSWORD,
-    database: dbConfig.DB,
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
 });
 
 // open the MySQL connection
